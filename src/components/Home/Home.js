c67b068e971ad8af3a27a6d397f45c746f38643d
tmonty312 <tmonty312@gmail.com>
2019-01-12 18:44:50 -0700

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+    constructor(props){
+        super()
+    }
+
+    render(){
+        return(
+            <div>
+                Home page is here!
+            </div>
+        )
+    }
+}
+export default Home;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Login extends Component{
+    constructor(props){
+        super()
+    }
+
+    render(){
+        return(
+            <div>
+                The Login page is here!
+            </div>
+        )
+    }
+}
+export default Login;
